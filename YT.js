b1ffa68c8cdf670ba8f5a8b815c6c5e6ee9ea506
Caio Passinho <154100415+taiyo4@users.noreply.github.com>
2025-04-10 11:34:20 -0300

    Update YT.js

--- YT.js
+++ YT.js
@@ -1,7 +1,1 @@
-document.getElementById("toggle-theme").addEventListener("click", () => {
-    document.body.classList.toggle("dark");
-  
-    const isDark = document.body.classList.contains("dark");
-    document.getElementById("toggle-theme").textContent = isDark ? "☀️ Modo Claro" : "🌙 Modo Escuro";
-  });
-  
+
